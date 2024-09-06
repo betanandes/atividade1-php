5c2afef062d351a7cbcc0e0cd60e24121be7c550
Roberta Fernandes <160965280+betanandes@users.noreply.github.com>
2024-09-05 23:18:00 -0300

    new commit

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+echo 'Dados do Usuário' . '<br>';
+echo 'Nome da Just a Girl: '. $nome . '<br>' .'Idade: '. $idade;
+
+phpinfo();
+
+?>
+
+</body>
+</html>
+
